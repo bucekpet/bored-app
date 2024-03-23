5c75c9f979d821476fdd0c16aa291c2edba12836
Jebingo <58402893+Jebingo@users.noreply.github.com>
2024-03-23 21:15:02 +0100

    re-design

--- src/App.jsx
+++ src/App.jsx
@@ -28,50 +28,63 @@ function App() {
 
   return (
     <>
-      <div className="container-sm mt-5 w-l-50">
-        <div className='mb-4'>
+      <div className='bg-gradient-custom vh-100'>
+
+        <div className="container-sm text-blue-light d-flex flex-column justify-content-center vh-100 vw-100">
 
           {/* Title */}
-          <h1>{activity?.activity}</h1>
+          <h1 className='text-blue-light fw-bold'>{activity?.activity}</h1>
 
 
-          {/* Type */}
-          <p className='fw-lighter fs-2'>{activity?.type}</p>
-
-          <div className="position-relative">
-            <div className="position-absolute end-0">
-              <ThemeToggler onThemeChange={setTheme} />
-
+          <div className="row">
+            <div className="col">
+              {/* Type */}
+              <p className='fw-lighter fs-2 text-blue-dark text-capitalize'>{activity?.type}</p>
             </div>
 
+            <div className="col">
+              {/* Participants */}
+              <p className='pt-2'>{
+                activity && activity.participants > 0 &&
+                Array.from({ length: activity.participants }).map((_, index) => (
+                  <i key={index} className="bi bi-person-fill"></i>
+                ))
+              }</p>
+            </div>
           </div>
 
-          {/* Participants */}
-          <p>{
-            activity && activity.participants > 0 &&
-            Array.from({ length: activity.participants }).map((_, index) => (
-              <i key={index} className="bi bi-person-fill"></i>
-            ))
-          }</p>
 
-          {/* Price */}
-          <div className='mb-3'>
-            Price
-            <ProgressBar value={1 - activity?.price} />
+
+
+
+
+          <div className='mb-4'>
+            <div className="row">
+              <div className="col">
+                {/* Price */}
+                Price
+                <ProgressBar value={1 - activity?.price} />
+              </div>
+
+              <div className="col">
+                {/* Accessibility */}
+                Accessbility
+                <ProgressBar value={1 - activity?.accessibility} />
+              </div>
+            </div>
+
+
+            {/* Link */}
+            <a href={activity?.link}></a>
           </div>
 
-          {/* Accessibility */}
-          <div className='mb-3'>
-            Accessbility
-            <ProgressBar value={1 - activity?.accessibility} />
+          {/* Button */}
+          <div className="d-grid">
+            <button onClick={getActivity} className='btn  btn-dark text-blue-light'>I'm bored!
+              <i className="mx-1 bi bi-emoji-neutral-fill"></i>
+            </button>
+
           </div>
-
-          {/* Link */}
-          <a href={activity?.link}></a>
-        </div>
-
-        <div className="d-grid mt-5">
-          <button onClick={getActivity} className={theme === 'dark' ? `btn btn-outline-primary` : `btn btn-primary`}>Get Activity</button>
         </div>
 
       </div>

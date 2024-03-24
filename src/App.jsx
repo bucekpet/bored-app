967fe50e89711ffe754d634ef5a6de88f3aaee8e
Jebingo <58402893+bucekpet@users.noreply.github.com>
2024-03-24 15:28:25 +0100

    favorites finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
-import 'bootstrap/dist/css/bootstrap.css'
 import { useEffect, useState } from 'react'
 import { ProgressBar } from './ProgressBar'
+import { FavoritesModal } from './FavoritesModal'
+import { ParticipantsIcons } from './ParticipantsIcons'
+import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import './styles.css'
@@ -8,12 +10,27 @@ import './styles.css'
 
 function App() {
   const [activity, setActivity] = useState(null)
-  const [favorites, setFavorites] = useState([])
 
+  // Get favorites from local storage
+  const [favorites, setFavorites] = useState(() => {
+    const localValue = localStorage.getItem("FAVORITES")
+    if (localValue == null) return []
+    return JSON.parse(localValue)
+  })
 
+  // Gets activity once on page load
   useEffect(() => {
+
     getActivity()
+
+    // Debug
+    // console.log(favorites)
   }, [])
+
+  // Store favorites in local storage on change
+  useEffect(() => {
+    localStorage.setItem("FAVORITES", JSON.stringify(favorites))
+  }, [favorites])
 
   const getActivity = () => {
     fetch('https://www.boredapi.com/api/activity')
@@ -26,45 +43,61 @@ function App() {
       })
   }
 
-  const addToFavorites = () => {
-    if (activity != null) {
+  function isInFavorites(activity) {
+    return favorites.some(favorite => favorite.activity === activity)
+  }
+
+  function addFavorite() {
+    if (activity != null && !isInFavorites(activity.activity)) {
       setFavorites((currentFavorites) => {
-        return [...currentFavorites, activity]
+        return [...currentFavorites, { id: crypto.randomUUID(), ...activity }]
       })
     }
+  }
 
-    console.log(favorites)
+  function deleteFavorite(id) {
+    setFavorites(currentFavorites => {
+      return currentFavorites.filter(fav => fav.id !== id)
+    })
+  }
+
+  function deleteFavoriteByActivity(activity) {
+    setFavorites(currentFavorites => {
+      return currentFavorites.filter(fav => fav.activity !== activity)
+    })
   }
 
   return (
     <>
       <div className='bg-gradient-custom vh-100'>
 
-        <div className="container-sm text-blue-light d-flex flex-column justify-content-center vh-100 vw-100">
+        <div className="position-absolute w-100 mt-2">
+          <div className="container">
+            <FavoritesModal favorites={favorites} deleteFavorite={deleteFavorite} />
+          </div>
+        </div>
+
+        <div className="container-sm text-blue-light d-flex flex-column justify-content-center vh-100 vw-100 fs-4">
+
 
           {/* Title */}
-          <h1 className='text-blue-light fw-bold'>{activity?.activity}</h1>
-
+          <h1 className='text-blue-light fw-bold user-select-none display-3'>{activity?.activity}</h1>
 
           <div className="row">
             <div className="col">
               {/* Type */}
-              <p className='fw-lighter fs-3 text-blue-dark text-capitalize'>{activity?.type}</p>
+              <p className='fw-lighter fs-2 text-blue-dark text-capitalize user-select-none'>{activity?.type}</p>
             </div>
 
+            {/* Participants */}
             <div className="col d-flex align-items-center">
-              {/* Participants */}
-              <p className=''>{
-                activity && activity.participants > 0 &&
-                Array.from({ length: activity.participants }).map((_, index) => (
-                  <i key={index} className="bi bi-person-fill"></i>
-                ))
-              }</p>
+              <ParticipantsIcons participants={activity?.participants} />
             </div>
+
           </div>
 
 
-          <div className='mb-4'>
+          <div className='mb-4 user-select-none'>
             <div className="row">
               <div className="col">
                 {/* Price */}
@@ -78,44 +111,38 @@ function App() {
                 <ProgressBar value={1 - activity?.accessibility} />
               </div>
             </div>
-
-
-            {/* Link */}
-            <a href={activity?.link}></a>
           </div>
 
           {/* Buttons */}
-          <div className="row">
-            <div className="col-10">
-              <div className="d-grid">
-                <button onClick={getActivity} className='btn  btn-dark text-blue-light'>I'm bored!
-                  <i className="mx-1 bi bi-emoji-neutral-fill"></i>
-                </button>
-              </div>
-            </div>
 
-            <div className="col-2 px-0 btn-group">
-              <button onClick={addToFavorites} className='btn btn-dark text-blue-light'>
-                <i className="bi bi-heart"></i>
+
+          <div className="row gx-2">
+            <div className="col-sm-10 col-9 col-lg-11 d-grid">
+              <button onClick={getActivity} className='btn fs-3 btn-dark text-blue-light'>I'm bored!
+                <i className="mx-1 bi bi-emoji-neutral-fill"></i>
               </button>
-
-              <button type="button" className="btn btn-dark dropdown-toggle dropdown-toggle-split text-blue-light" data-bs-toggle="dropdown" aria-expanded="false" />
-              <span className="visually-hidden">Toggle Dropdown</span>
             </div>
 
-            <ul className="dropdown-menu">
-              <li><a className="dropdown-item" href="#">Action</a></li>
-              <li><a className="dropdown-item" href="#">Another action</a></li>
-              <li><a className="dropdown-item" href="#">Something else here</a></li>
-              <li><a className="dropdown-item" href="#">Separated link</a></li>
-            </ul>
+            <div className="col d-grid">
+              <button onClick={() => {
+                if (isInFavorites(activity?.activity)) {
+                  return deleteFavoriteByActivity(activity.activity)
+                } else {
+                  return addFavorite()
+                }
+              }} className='btn fs-2 btn-outline-dark text-blue-light'>
+                <i className={`bi ${isInFavorites(activity?.activity) ? "bi-suit-heart-fill" : "bi-suit-heart"}`}></i>
+              </button>
+            </div>
           </div>
 
-          <div className='fixed-bottom d-flex justify-content-center pb-2'>
-            <a href="https://github.com/bucekpet/" target="_blank">
-              <i className="bi bi-github"></i>
-            </a>
-          </div>
+        </div>
+
+        {/* Footer */}
+        <div className='fixed-bottom d-flex justify-content-center py-1'>
+          <a href="https://github.com/bucekpet/" target="_blank">
+            <i className="bi fs-3 bi-github"></i>
+          </a>
         </div>
       </div >
 
--- src/FavoritesModal.jsx
+++ src/FavoritesModal.jsx
@@ -1,22 +1,42 @@
-export function FavoritesModal() {
+import { FavoriteItem } from "./FavoriteItem"
+
+export function FavoritesModal({ favorites, deleteFavorite }) {
 
     return (
-        <div class="modal" tabindex="-1">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title">Modal title</h5>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <p>Modal body text goes here.</p>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
+        <>
+            <div className="d-flex justify-content-start">
+
+                <button type="button" className="btn fs-5 text-blue-light p-0" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Favorites <i className="bi bi-suit-heart-fill" style={{ fontSize: '1rem' }}></i>
+                </button>
+            </div>
+
+            <div className="modal fade text-blue-light" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h1 className="modal-title fs-5" id="exampleModalLabel">Favorites</h1>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div className="modal-body">
+
+                            {favorites.length === 0 && "No favorites"}
+
+                            <ul className="list-group list-group-flush">
+                                {favorites.map((fav) => {
+                                    return <FavoriteItem {...fav} key={fav.id} deleteFavorite={deleteFavorite} />
+                                })}
+                            </ul>
+
+
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-outline-secondary text-blue-light" data-bs-dismiss="modal">Close</button>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
+
     )
 }
